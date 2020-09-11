3eeba5d7b5ad53ab65d90e8a7cc31f6fa73341e2
Joshua Singleton <jsingle345@gmail.com>
2020-09-11 23:38:20 +0000

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -28,16 +28,13 @@ def play(songs)
   
   
   if songs.include? user_response
-    puts "Playing #{songs.find |element| element == user_response}"
-  
-end
-  
-  #   else 
-  #   puts "Invalid input, please try again"
-  #   i += 1
-  #   end
-  # end
-  # hits
+    puts "Playing #{songs.find {|element| element == user_response}}"
+  elsif (1..9).to_a.include?(user_response.to_i)
+    puts "Playing #{songs[user_response.to_i - 1]}"
+  else 
+    puts "Invalid input, please try again"
+  end
+  songs
 end
 
 def list(songs)
@@ -51,22 +48,50 @@ def exit_jukebox
   puts "Goodbye"
 end
 
-def run 
-  puts "Please enter a command:" 
-  input = gets.strip
-  
-  if input == help
-    help 
-  elsif input == list
-    list(songs)
-  elsif input == play 
-    play(songs)
-  else 
-    input == exit 
-  exit_jukebox
+# def run(my_songs)
+# help
+#   puts "Please enter a command:"
+#   user_response = gets.chomp
+#   while user_response != "exit"
+#     case user_response
+#       when "help"
+#         help
+#         puts "Please enter a command:"
+#         user_response = gets.chomp
+#       when "list"
+#         list(my_songs)
+#         puts "Please enter a command:"
+#         user_response = gets.chomp
+#       when "play"
+#         play(my_songs)
+#         puts "Please enter a command:"
+#         user_response = gets.chomp
+#       when "exit"
+#         exit_jukebox
+#       else
+#         puts "Invalid input, please try again:"
+#         user_response = gets.chomp
+#       end
+#     end
+# end 
+
+def run(my_songs)
+  input = ""
+  while input != "exit"
+    puts "Please enter a command:"
+    input = gets.chomp
+    case input
+    when "play"
+      play(my_songs)
+    when "list"
+      list(my_songs)
+    when "help"
+      help
+    when "exit"
+      exit_jukebox    
+    else
+      puts "Invalid input, please try again"
+    end
   end
-  
-  while input != exit do
-  run
-  end
-end
+end	
+
